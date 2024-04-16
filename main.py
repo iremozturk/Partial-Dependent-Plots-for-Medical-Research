8e91e829df0b7d3a1744c227cdbbc737b1b3140c
iremozturk <iremozturk97@hotmail.com>
2024-04-16 15:11:12 -0700

    Datasets from 12 to 22 preprocessed and used for model training

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
 from model_training import train_and_plot
 
 if __name__ == "__main__":
-    dataset_number = "12"   # Change this to the desired dataset number
+    dataset_number = "20"   # Change this to the desired dataset number
     train_and_plot(dataset_number, num_top_features=1)
